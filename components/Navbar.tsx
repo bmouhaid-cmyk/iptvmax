a96fc1668555d75a8a6060cbfc67de95f38eeb94
elgitano1995 <b.mouhaid@gmail.com>
2026-02-03 15:58:49 +0100

    Refine navbar height

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -37,7 +37,7 @@ export default function Navbar() {
     return (
         <nav className="bg-slate-900 border-b border-slate-800">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-                <div className="flex items-center justify-between h-20">
+                <div className="flex items-center justify-between h-[72px]">
                     <div className="flex items-center">
                         <Link href="/" className="flex items-center">
                             <Image
